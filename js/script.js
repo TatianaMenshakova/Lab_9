f399a03079e7536da2ccb3ccd73a5ffbce95d292
TatianaMenshakova <tanyaaamenshakova@gmail.com>
2025-06-26 20:05:03 +0300

    adding initial js code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
+		
+		var entryButton = document.getElementById('entrybutton');
+		var entryinput = document.getElementById('entryinput');
+    
+		entryButton.addEventListener('click', function() {
+		text = input.value;
+    alert('Tatiana Menshakova:');
+  });
+}
 
 
 window.addEventListener('load', init);

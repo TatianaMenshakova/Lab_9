200536b07507dc3d72c48d8f87667708bc91d4d7
TatianaMenshakova <tanyaaamenshakova@gmail.com>
2025-06-27 08:24:22 +0300

    removing an extra line

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ function init(){
 		var textbox = document.getElementById('entryinput');
 		document.getElementById('textoutput').innerHTML = textbox.value;
     alert('Tatiana Menshakova: ' + textbox.value);
-		textOutput.textContent = message;
   }
 	
 	button.addEventListener('click', showMeText);
 	}
+	
 window.addEventListener('load', init);
